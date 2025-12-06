5420c3e78fc25a4a138b6ab15b7e63d9015e5597
TonyBarretor <tony.barretor@gmail.com>
2025-12-06 11:40:51 -0800

    Add 'this' explanation

--- src/App.jsx
+++ src/App.jsx
@@ -2,55 +2,10 @@
 
 function App () {
 
-// LET VS CONST - use in ES6
-// VAR - you can acces from outside the block
-//LET - only can access inside the block
-
-
-  /* function sayHello() {
-  for (let i = 0; i < 5; i++) { //ONLY Use let if you need to reassing the variable
-    console.log(i);
-  }
-
-  console.log(i);
-  
-}
-
-sayHello(); */
-
-// const is used to assign values to a variable but its value
-//will be CONSTANT
-/* const x = 1;
-x = 2; */
-
-// OBJECTSSSSSSSSSSSSS
-// It is a collection of value pairs
-
-const person = {
-  name: 'Pika', //property
-  //walk: function() {} //When a function is inside an object is called METHOD
-//above is an old way to set up a method
-
-//This is ES6 way to set a method
-  walk() {},
-  talk() {},
-};
-
-//THERe is 2 ways to access those members
-
-//Dot notation
-person.talk(); //dot notation - Use when you know what property you are going to use
-person.name = 'Graciela';
-
-//Braket notation 
-person['name'] = 'John'; //braket notation - use this when you dont know ahead of time what property or method to we are going to access
-//What goes inside the brackets determine what property or member you are going to use
-
-const targetMember = 'name'; //Imagine targetMember is a field to fill in a form
-person[targetMember.value] = 'Goku'; //so you can access writing inside the brackets the property or method 
 
 //THIS KEY WORDDDDDDDDDDDD
-
+//'this' reference to the current object, but that always dont work like that
+//'this' value is determined how the function is called
 const persona = {
   name: 'Mosh',
   walk() {
@@ -58,7 +13,14 @@ const persona = {
   }
 };
 
-persona.walk();
+persona.walk(); //here walk() is a function called as a method, this will return a reference to the Object 'persona'
+
+const walk = persona.walk; //this is calling to reference the function 'walk' in this line
+//Remember to call a method use the parenthesis () after the method name
+
+console.log(walk);
+walk(); //calling the function as a standalone object or outside of an object, this will return the global object called Window object, because we are in react there is no window object so we get undefined because of the STRICT MODE (run javascript safely)
+
 
 }
 
